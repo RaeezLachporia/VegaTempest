85b3eedadbc54591e9d84087031cb6c24c0f73bd
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2023-05-29 17:31:34 +0200

    added new scripts

---
+++ VegaTempest/Assets/Scripts/miniMaxTree.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniMaxTree : MonoBehaviour
+{
+    public class node
+    {
+        public BattleSystem BSystem { get; set; }
+
+        public List<node> Children;
+        public node(BattleSystem bsystem)
+        {
+            BSystem = bsystem;
+            Children = new List<node>();
+        }
+    }
+    private void generateRecursiveTree(node parent, int depth, bool playermaximizing)
+    {
+        if (depth == 0 && parent.BSystem.State = BattleStates.Lose)
+        {
+            return;
+        }
+
+        List
+    }
+    public node createTree(BattleSystem battleSystem, int depth)
+    {
+        node Root = new node(battleSystem);
+        generateRecursiveTree(Root, depth, true);
+        return Root;
+    }
+}
